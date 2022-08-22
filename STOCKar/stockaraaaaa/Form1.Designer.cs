57ce48ae0274d7f91f0e68ceaf9fe82e6c93b3b9
Mariano Angel Cavallera <75549353+marianocavallera@users.noreply.github.com>
2022-08-22 15:53:31 -0300

    STOCKar
    Software de generacion de archivos .docx que sirvio como generador de autorizacion de porte de equipos informaticos.

---
+++ STOCKar/stockaraaaaa/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace stockaraaaaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxINC = new System.Windows.Forms.TextBox();
+            this.boxNombre = new System.Windows.Forms.TextBox();
+            this.boxTeclado = new System.Windows.Forms.TextBox();
+            this.boxMonitor = new System.Windows.Forms.TextBox();
+            this.boxCargador = new System.Windows.Forms.TextBox();
+            this.boxMouse = new System.Windows.Forms.TextBox();
+            this.boxHeadset = new System.Windows.Forms.TextBox();
+            this.boxToken = new System.Windows.Forms.TextBox();
+            this.boxActivo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxActivo = new System.Windows.Forms.ComboBox();
+            this.checkTeclado = new System.Windows.Forms.CheckBox();
+            this.checkMonitor = new System.Windows.Forms.CheckBox();
+            this.checkCargador = new System.Windows.Forms.CheckBox();
+            this.checkMouse = new System.Windows.Forms.CheckBox();
+            this.checkHeadset = new System.Windows.Forms.CheckBox();
+            this.checkToken = new System.Windows.Forms.CheckBox();
+            this.snActivo = new System.Windows.Forms.TextBox();
+            this.labelStockar = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxModelo = new System.Windows.Forms.ComboBox();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.btnRuta = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENVIAR PEDIDO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(577, 13);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "_________________________________________________________________________________" +
+    "______________";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(263, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "INC/RQST/OSS/TASK/CAT/RITM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "NOMBRE:";
+            // 
+            // boxINC
+            // 
+            this.boxINC.Location = new System.Drawing.Point(450, 77);
+            this.boxINC.Name = "boxINC";
+            this.boxINC.Size = new System.Drawing.Size(186, 20);
+            this.boxINC.TabIndex = 33;
+            // 
+            // boxNombre
+            // 
+            this.boxNombre.Location = new System.Drawing.Point(450, 42);
+            this.boxNombre.Name = "boxNombre";
+            this.boxNombre.Size = new System.Drawing.Size(186, 20);
+            this.boxNombre.TabIndex = 34;
+            // 
+            // boxTeclado
+            // 
+            this.boxTeclado.Location = new System.Drawing.Point(247, 368);
+            this.boxTeclado.Name = "boxTeclado";
+            this.boxTeclado.Size = new System.Drawing.Size(225, 20);
+            this.boxTeclado.TabIndex = 50;
+            // 
+            // boxMonitor
+            // 
+            this.boxMonitor.Location = new System.Drawing.Point(247, 336);
+            this.boxMonitor.Name = "boxMonitor";
+            this.boxMonitor.Size = new System.Drawing.Size(225, 20);
+            this.boxMonitor.TabIndex = 49;
+            // 
+            // boxCargador
+            // 
+            this.boxCargador.Location = new System.Drawing.Point(247, 303);
+            this.boxCargador.Name = "boxCargador";
+            this.boxCargador.Size = new System.Drawing.Size(225, 20);
+            this.boxCargador.TabIndex = 48;
+            // 
+            // boxMouse
+            // 
+            this.boxMouse.Location = new System.Drawing.Point(247, 272);
+            this.boxMouse.Name = "boxMouse";
+            this.boxMouse.Size = new System.Drawing.Size(225, 20);
+            this.boxMouse.TabIndex = 47;
+            // 
+            // boxHeadset
+            // 
+            this.boxHeadset.Location = new System.Drawing.Point(247, 239);
+            this.boxHeadset.Name = "boxHeadset";
+            this.boxHeadset.Size = new System.Drawing.Size(225, 20);
+            this.boxHeadset.TabIndex = 46;
+            // 
+            // boxToken
+            // 
+            this.boxToken.Location = new System.Drawing.Point(247, 207);
+            this.boxToken.Name = "boxToken";
+            this.boxToken.Size = new System.Drawing.Size(225, 20);
+            this.boxToken.TabIndex = 45;
+            // 
+            // boxActivo
+            // 
+            this.boxActivo.Location = new System.Drawing.Point(196, 154);
+            this.boxActivo.Name = "boxActivo";
+            this.boxActivo.Size = new System.Drawing.Size(129, 20);
+            this.boxActivo.TabIndex = 44;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(143, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "ACTIVO";
+            // 
+            // comboBoxActivo
+            // 
+            this.comboBoxActivo.FormattingEnabled = true;
+            this.comboBoxActivo.Items.AddRange(new object[] {
+            "LAPTOP",
+            "DESKTOP",
+            "LAPTOP SWING",
+            "DESKTOP SWING"});
+            this.comboBoxActivo.Location = new System.Drawing.Point(196, 127);
+            this.comboBoxActivo.Name = "comboBoxActivo";
+            this.comboBoxActivo.Size = new System.Drawing.Size(129, 21);
+            this.comboBoxActivo.TabIndex = 42;
+            this.comboBoxActivo.Text = "SELECCIONAR TIPO";
+            this.comboBoxActivo.SelectedIndexChanged += new System.EventHandler(this.comboBoxActivo_SelectedIndexChanged);
+            // 
+            // checkTeclado
+            // 
+            this.checkTeclado.AutoSize = true;
+            this.checkTeclado.Location = new System.Drawing.Point(156, 370);
+            this.checkTeclado.Name = "checkTeclado";
+            this.checkTeclado.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkTeclado.Size = new System.Drawing.Size(76, 17);
+            this.checkTeclado.TabIndex = 41;
+            this.checkTeclado.Text = "TECLADO";
+            this.checkTeclado.UseVisualStyleBackColor = true;
+            // 
+            // checkMonitor
+            // 
+            this.checkMonitor.AutoSize = true;
+            this.checkMonitor.Location = new System.Drawing.Point(156, 338);
+            this.checkMonitor.Name = "checkMonitor";
+            this.checkMonitor.Size = new System.Drawing.Size(77, 17);
+            this.checkMonitor.TabIndex = 40;
+            this.checkMonitor.Text = "MONITOR";
+            this.checkMonitor.UseVisualStyleBackColor = true;
+            // 
+            // checkCargador
+            // 
+            this.checkCargador.AutoSize = true;
+            this.checkCargador.Location = new System.Drawing.Point(156, 306);
+            this.checkCargador.Name = "checkCargador";
+            this.checkCargador.Size = new System.Drawing.Size(87, 17);
+            this.checkCargador.TabIndex = 39;
+            this.checkCargador.Text = "CARGADOR";
+            this.checkCargador.UseVisualStyleBackColor = true;
+            // 
+            // checkMouse
+            // 
+            this.checkMouse.AutoSize = true;
+            this.checkMouse.Location = new System.Drawing.Point(156, 274);
+            this.checkMouse.Name = "checkMouse";
+            this.checkMouse.Size = new System.Drawing.Size(65, 17);
+            this.checkMouse.TabIndex = 38;
+            this.checkMouse.Text = "MOUSE";
+            this.checkMouse.UseVisualStyleBackColor = true;
+            // 
+            // checkHeadset
+            // 
+            this.checkHeadset.AutoSize = true;
+            this.checkHeadset.Location = new System.Drawing.Point(156, 242);
+            this.checkHeadset.Name = "checkHeadset";
+            this.checkHeadset.Size = new System.Drawing.Size(77, 17);
+            this.checkHeadset.TabIndex = 37;
+            this.checkHeadset.Text = "HEADSET";
+            this.checkHeadset.UseVisualStyleBackColor = true;
+            // 
+            // checkToken
+            // 
+            this.checkToken.AutoSize = true;
+            this.checkToken.Location = new System.Drawing.Point(156, 209);
+            this.checkToken.Name = "checkToken";
+            this.checkToken.Size = new System.Drawing.Size(63, 17);
+            this.checkToken.TabIndex = 36;
+            this.checkToken.Text = "TOKEN";
+            this.checkToken.UseVisualStyleBackColor = true;
+            // 
+            // snActivo
+            // 
+            this.snActivo.Location = new System.Drawing.Point(331, 154);
+            this.snActivo.Name = "snActivo";
+            this.snActivo.Size = new System.Drawing.Size(153, 20);
+            this.snActivo.TabIndex = 51;
+            // 
+            // labelStockar
+            // 
+            this.labelStockar.AutoSize = true;
+            this.labelStockar.Font = new System.Drawing.Font("Tahoma", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStockar.Location = new System.Drawing.Point(12, 28);
+            this.labelStockar.Name = "labelStockar";
+            this.labelStockar.Size = new System.Drawing.Size(236, 58);
+            this.labelStockar.TabIndex = 52;
+            this.labelStockar.Text = "STOCKar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "TM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(577, 13);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "_________________________________________________________________________________" +
+    "______________";
+            // 
+            // comboBoxModelo
+            // 
+            this.comboBoxModelo.FormattingEnabled = true;
+            this.comboBoxModelo.Location = new System.Drawing.Point(331, 127);
+            this.comboBoxModelo.Name = "comboBoxModelo";
+            this.comboBoxModelo.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxModelo.TabIndex = 56;
+            this.comboBoxModelo.Text = "SELECCIONAR MODELO";
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(12, 462);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(115, 13);
+            this.lblRuta.TabIndex = 57;
+            this.lblRuta.Text = "SELECCIONAR RUTA";
+            this.lblRuta.TextChanged += new System.EventHandler(this.lblRuta_TextChanged);
+            // 
+            // btnRuta
+            // 
+            this.btnRuta.Location = new System.Drawing.Point(12, 431);
+            this.btnRuta.Name = "btnRuta";
+            this.btnRuta.Size = new System.Drawing.Size(124, 28);
+            this.btnRuta.TabIndex = 58;
+            this.btnRuta.Text = "SELECCIONAR RUTA";
+            this.btnRuta.UseVisualStyleBackColor = true;
+            this.btnRuta.Click += new System.EventHandler(this.btnRuta_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "SELECCIONE PLANTILLA DE TERMINOS DE ASIGNACION";
+            this.openFileDialog1.Filter = "SELECCIONE PLANTILLA DE TERMINOS DE ASIGNACION(*.docx)|*.docx";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 475);
+            this.Controls.Add(this.btnRuta);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.comboBoxModelo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelStockar);
+            this.Controls.Add(this.snActivo);
+            this.Controls.Add(this.boxTeclado);
+            this.Controls.Add(this.boxMonitor);
+            this.Controls.Add(this.boxCargador);
+            this.Controls.Add(this.boxMouse);
+            this.Controls.Add(this.boxHeadset);
+            this.Controls.Add(this.boxToken);
+            this.Controls.Add(this.boxActivo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxActivo);
+            this.Controls.Add(this.checkTeclado);
+            this.Controls.Add(this.checkMonitor);
+            this.Controls.Add(this.checkCargador);
+            this.Controls.Add(this.checkMouse);
+            this.Controls.Add(this.checkHeadset);
+            this.Controls.Add(this.checkToken);
+            this.Controls.Add(this.boxNombre);
+            this.Controls.Add(this.boxINC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "STOCKar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox boxINC;
+        private System.Windows.Forms.TextBox boxNombre;
+        private System.Windows.Forms.TextBox boxTeclado;
+        private System.Windows.Forms.TextBox boxMonitor;
+        private System.Windows.Forms.TextBox boxCargador;
+        private System.Windows.Forms.TextBox boxMouse;
+        private System.Windows.Forms.TextBox boxHeadset;
+        private System.Windows.Forms.TextBox boxToken;
+        private System.Windows.Forms.TextBox boxActivo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxActivo;
+        private System.Windows.Forms.CheckBox checkTeclado;
+        private System.Windows.Forms.CheckBox checkMonitor;
+        private System.Windows.Forms.CheckBox checkCargador;
+        private System.Windows.Forms.CheckBox checkMouse;
+        private System.Windows.Forms.CheckBox checkHeadset;
+        private System.Windows.Forms.CheckBox checkToken;
+        private System.Windows.Forms.TextBox snActivo;
+        private System.Windows.Forms.Label labelStockar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxModelo;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.Button btnRuta;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ STOCKar/stockaraaaaa/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
+using STOCKar.Properties;
+
+namespace stockaraaaaa
+{
+    public partial class Form1 : Form
+    {
+
+        string INC;
+        string nombre;
+        string activo;
+        string token;
+        string headset;
+        string mouse;
+        string cargador;
+        string monitor;
+        string teclado;
+        int i = 0;
+        string rutao;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        // Change the text in a table in a word processing document.
+        public void ChangeTextInCell(string filepath)
+        {
+
+
+            this.INC = boxINC.Text;
+            this.nombre = boxNombre.Text;
+            this.activo = boxActivo.Text;
+            this.token = boxToken.Text;
+            this.headset = boxHeadset.Text;
+            this.mouse = boxMouse.Text;
+            this.cargador = boxCargador.Text;
+            this.monitor = boxMonitor.Text;
+            this.teclado = boxTeclado.Text;
+            string complemento = "";
+
+            string[] texts = new string[] { comboBoxActivo.Text, comboBoxModelo.Text, "HP", boxActivo.Text, snActivo.Text, "" };
+
+
+
+            if (checkToken.Checked)
+            {
+                complemento += " TOKEN: " + token;
+            }
+            if (checkHeadset.Checked)
+            {
+                complemento += "\n HEADSET: " + headset;
+            }
+            if (checkMouse.Checked)
+            {
+                complemento += "\n MOUSE: " + mouse;
+            }
+            if (checkCargador.Checked)
+            {
+                complemento += "\n CARGADOR: " + cargador;
+            }
+            if (checkMonitor.Checked)
+            {
+                complemento += "\n MONITOR: " + monitor;
+            }
+            if (checkTeclado.Checked)
+            {
+                complemento += "\n  TECLADO: " + teclado;
+            }
+            texts[5] = complemento;
+
+
+            DateTime fechaActual = DateTime.Now;
+            string newWord = filepath.Replace(".docx", " - " + nombre + " - " + activo + " - " + INC + ".docx");
+            File.Copy(filepath, newWord);
+
+            // Use the file name and path passed in as an argument to 
+            // open an existing document.            
+            using (WordprocessingDocument doc =
+                WordprocessingDocument.Open(newWord, true))
+            {
+
+                // Find the first table in the document.
+                Table table =
+                    doc.MainDocumentPart.Document.Body.Elements<Table>().First();
+                for (i = 0; i < texts.Length; i++)
+                {
+                    // Find the second row in the table.
+                    TableRow row = table.Elements<TableRow>().ElementAt(i);
+
+                    // Find the second cell in the row.
+                    TableCell cell = row.Elements<TableCell>().ElementAt(1);
+
+                    // Find the first paragraph in the table cell.
+                    Paragraph p = cell.Elements<Paragraph>().First();
+
+                    // Find the first run in the paragraph.
+                    Run r = p.Elements<Run>().First();
+
+                    // Set the text for the run.
+                    Text t = r.Elements<Text>().First();
+                    t.Text = texts[i];
+                }
+
+            }
+            //CAMBIO DE NOMBRE
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(newWord, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(doc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                Regex regexText = new Regex("nome");
+                docText = regexText.Replace(docText, boxNombre.Text);
+
+                using (StreamWriter sw = new StreamWriter(doc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+            }
+        }
+
+      
+        //REMPLAZO DE TEXTO Y DECLARACION DE RUTA
+        public void button1_Click(object sender, EventArgs e)
+        {
+           // string rutax = @"C:\Users\Marianito\Desktop\word\New folder\Termino de Asignacion.docx";
+
+            ChangeTextInCell(rutao);
+        }
+        //MODELOS
+        private void comboBoxActivo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxModelo.Items.Clear();
+
+            if (comboBoxActivo.Text == "LAPTOP")
+            {
+                comboBoxModelo.Items.Add("G2 645");
+                comboBoxModelo.Items.Add("G3 645");
+                comboBoxModelo.Items.Add("G3 725");
+                comboBoxModelo.Items.Add("G3 820");
+                comboBoxModelo.Items.Add("G4 645");
+                comboBoxModelo.Items.Add("G6 745");
+                comboBoxModelo.Items.Add("G6 830");
+                comboBoxModelo.Items.Add("G7 845");
+            }
+
+            if (comboBoxActivo.Text == "DESKTOP")
+            {
+                comboBoxModelo.Items.Add("G2 705");
+                comboBoxModelo.Items.Add("G3 705");
+                comboBoxModelo.Items.Add("G5 705");
+            }
+            if (comboBoxActivo.Text == "DESKTOP SWING")
+            {
+                comboBoxModelo.Items.Add("Z400");
+                comboBoxModelo.Items.Add("Z440");
+                comboBoxModelo.Items.Add("Z4");
+            }
+            if (comboBoxActivo.Text == "LAPTOP SWING")
+            {
+                comboBoxModelo.Items.Add("G2 ZBOOK");
+                comboBoxModelo.Items.Add("G3 ZBOOK");
+                comboBoxModelo.Items.Add("G6 ZBOOK");
+                comboBoxModelo.Items.Add("G7 ZBOOK");
+            }
+        }
+        //GUARDAR RUTA
+        private void btnRuta_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblRuta.Text = openFileDialog1.FileName;
+                this.rutao = lblRuta.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRuta.Text = (string)STOCKar.Properties.Settings.Default["rutaGuardada"];
+        }
+
+        private void lblRuta_TextChanged(object sender, EventArgs e)
+        {
+            STOCKar.Properties.Settings.Default["rutaGuardada"] = lblRuta.Text;
+            STOCKar.Properties.Settings.Default.Save();
+        }
+    }
+}
+
+
+
+
+
